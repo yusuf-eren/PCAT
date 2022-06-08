d5cd64f25e2e6d2288a57cf25220938ef32078f4
Yusuf Eren <erenyusuf170@gmail.com>
2022-06-08 22:26:11 +0300

    updated All

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const fn = function () {
-  console.log('function working');
-};
-fn()
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`http://localhost:${port} adresinde başlatıldı`);
+});
